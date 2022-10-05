e562d880a561b9153d44a285aa3694e992485767
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-06 02:51:32 +0300

    keep only primer-blast like tm calculation

--- tm_calculator_gui.py
+++ tm_calculator_gui.py
@@ -8,31 +8,18 @@ def main():
 	ap = GooeyParser()
 	ap.add_argument("-forward", "--forward", required=True, type=str, help="forward primer sequence")
 	ap.add_argument("-reverse", "--reverse", required=True, type=str, help="forward primer sequence")
-	ap.add_argument("-progam", "--program", required=False, type=int, default=1, help="program to choose(1.classic Tm calculation, 2.Calculation based on nearest neighbor thermodynamics). Default is 1")
-	ap.add_argument("-Na", "--Na", required=False, type=float, default=50.0, help="Na+ concentration(float number)")
-	ap.add_argument("-Mg", "--Mg", required=False, type=float, default=1.5, help="Mg2+ concentration(float number)")
-	ap.add_argument("-dNTPs", "--dNTPs", required=False, type=float, default=0.6, help="dNTPs concentration(float number)")
+	ap.add_argument("-Na", "--Na", required=False, type=float, default=50.0, help="Na+ concentration")
+	ap.add_argument("-Mg", "--Mg", required=False, type=float, default=1.5, help="Mg2+ concentration")
+	ap.add_argument("-dNTPs", "--dNTPs", required=False, type=float, default=0.6, help="dNTPs concentration")
 	args = vars(ap.parse_args())
-# create the function to calculate the Tm:
-	def tm(seq):
-		return 2 * seq.count("A") + 2 * seq.count("C") + 4 * seq.count("G") + 4 * seq.count("C")
-# main
 # import forward primer
 	frwd = Seq(args['forward'])
 # import reverse primer
 	rever = Seq(args['reverse'])
-# choose program
-	program = args['program']
-	if program == 1:
 # calculate Tm of forward primer
-		print("The Tm of the forward primer is:",tm(frwd),sep=" ")
+	print("The Tm of the forward primer is:",round(mt.Tm_NN(frwd, Na=args['Na'], Mg=args['Mg'], dNTPs=args['dNTPs']), 2),sep=" ")
 # calculate Tm of reverse primer
-		print("The Tm of the reverse primer is:",tm(rever),sep=" ")
-	elif program == 2:
-# calculate Tm of forward primer
-		print("The Tm of the forward primer is:",'%0.2f' % mt.Tm_NN(frwd, Na=args['Na'], Mg=args['Mg'], dNTPs=args['dNTPs']),sep=" ")
-# calculate Tm of reverse primer
-		print("The Tm of the reverse primer is:",'%0.2f' % mt.Tm_NN(rever, Na=args['Na'], Mg=args['Mg'], dNTPs=args['dNTPs']),sep=" ")
+	print("The Tm of the reverse primer is:",round(mt.Tm_NN(rever, Na=args['Na'], Mg=args['Mg'], dNTPs=args['dNTPs']), 2),sep=" ")
 
 if __name__ == '__main__':
 	main()
